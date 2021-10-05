cc28167f3e702a6b1f36b07ea30aa396f1a271db
CAZPFITL <cazpfitl@gmail.com>
2021-10-05 04:28:10 -0500

    just 4 fun

--- src/AntsApp.js
+++ src/AntsApp.js
@@ -67,6 +67,7 @@ export default class AntsApp {
     welcomeToAnts() {
         this.world = new World('sunny')
         this.anthill = new Anthill()
+        this.showOff()
     }
     
     showOff() {
